1b06c3530e5ca98097cac51d3b061de584c0dd5b
fezhull <felizhielerfezull@gmail.com>
2025-09-25 19:01:24 +0300

    Update Hero section with resume button and download button

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion';
+import { HiOutlineViewGrid, HiDownload } from "react-icons/hi";
+
+
 export const Hero = () => {
 
     //Icons array 
@@ -53,6 +56,7 @@ export const Hero = () => {
                             <Image
                             src={icon}
                             key={index}
+                            alt={`icon ${index + 1}`}
                             width={40}
                             height={40}
                             className='md:w-[50px] md:h-[50px]'
@@ -61,6 +65,11 @@ export const Hero = () => {
                         ))}
                     </motion.div>
 
+                </div>
+                {/* Buttons */}
+                <div className='flex flex-col md:flex-row items-center gap-4 w-full md:w-auto'>
+                    <a href="#portfolio" className='custom-gradient w-full md:w-auto py-3 px-8 text-white rounded-full font-semibold flex items-center justify-center gap-2 hover:opacity-90 transition-opacity duration-300'>My Work <HiOutlineViewGrid className='text-lg'/> </a>
+                    <a href="#/assets/resume.pdf" download className='w-full md:w-auto py-3 px-8 rounded-full font-semibold border border-content/20 hover:border-content/40 hover:shadow-sm transition-colors duration-300 flex items-center justify-center gap-2'>My Resume <HiDownload className='text-lg'/></a>
                 </div>
             </div>
         </section>
